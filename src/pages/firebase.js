b6aceb8acbd61b6fc464f0f2cc28b8eddac11af1
mukhlisrambe5 <mukhlis.rambe5@gmail.com>
2023-08-01 20:48:34 +0700

    add firebase js

---
+++ src/pages/firebase.js
@@ -0,0 +1,21 @@
+import firebase from"firebase/compat/app";
+import  "firebase/compat/database";
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyCJUuCpPiYyzHfcyy6MmAKZhbXzd9VbdrQ",
+  
+    authDomain: "react-contact-dfe5e.firebaseapp.com",
+  
+    projectId: "react-contact-dfe5e",
+  
+    storageBucket: "react-contact-dfe5e.appspot.com",
+  
+    messagingSenderId: "142407081415",
+  
+    appId: "1:142407081415:web:8e8bc5b14d6dfb4669cde2"
+  
+  };
+
+
+  
